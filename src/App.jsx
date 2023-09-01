fd60c8ef32d372d73646d592b82ca4f1c8b290e5
Andy Alvarado <andyalvarado1997@gmail.com>
2023-09-01 13:15:52 -0500

    Init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { MindARThree } from 'mind-ar/dist/mindar-image-three.prod.js';
+
+function App() {
+    
+
+    return (
+        <>
+            <h1>Just for testing purposes</h1>
+        </>
+    )
+}
+
+export default App
